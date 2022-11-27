4758bb07918025d8965031fe0b091871d71b22de
karimoff96 <doniyorkarimoff96@gmail.com>
2022-11-27 14:24:53 +0500

    Connecting to sqlalchemy and CRUD func added

--- main.py
+++ main.py
@@ -1,22 +1,105 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
 import smtplib
 from environs import Env
 
 env = Env()
 env.read_env()
 
-posts = requests.get(env.str('url')).json()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 OWN_EMAIL = env.str('OWN_EMAIL')  # Receivers gmail address
 OWN_PASSWORD = env.str('OWN_PASSWORD')  # Google App password NOT gmail password
+TARGET_EMAIL = env.str('TARGET_EMAIL')  # Target email address
+
+##CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+# db.create_all()
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+@app.route('/')
+def get_all_posts():
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html', all_posts=posts)
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    requested_post = BlogPost.query.get(post_id)
+    return render_template("post.html", post=requested_post)
 
+@app.route("/new-post", methods=["GET", "POST"])
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=form.author.data,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
+
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
+
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post_to_delete = BlogPost.query.get(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
 
 @app.route('/about')
 def about():
@@ -35,26 +118,16 @@ def contact():
     return render_template('contact.html', msg_sent=False)
 
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    for post in posts:
-        if post['id'] == post_id:
-            post_body = post['body']
-            post_image = post['background']
-            post_title = post['title']
-            post_subtitle = post['subtitle']
-            return render_template('post.html', p_body=post_body, p_image=post_image, p_title=post_title,
-                                   p_sub=post_subtitle)
-
 
 def send_email(name, email, phone, message):
     '''In order to use SMPTLIB, you have to give permission to receivers gmail, acception external mails. FOr this turn on TWO STEP VERIFICATION on accaunt settings and create new app. user given 16 char app`s password as host email`s password'''
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(OWN_EMAIL, OWN_PASSWORD)
         connection.sendmail(from_addr=email,
-                            to_addrs=OWN_EMAIL,
+                            to_addrs=TARGET_EMAIL,
                             msg=email_message)
 
 
